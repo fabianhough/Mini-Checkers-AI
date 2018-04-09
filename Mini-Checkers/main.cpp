a8a133621d3da17e285ad10c172808adb936a4c5
Fabian Hough <32251754+fabianhough@users.noreply.github.com>
2018-04-09 01:38:08 -0400

    Added some UI Elements, Fixed AI Bug
    Added some formatting, and level select/player order
    Fixed AI bug where pointer to the board wasn't passing into the function; Error was cause by not passing pointer by reference.

--- Mini-Checkers/main.cpp
+++ Mini-Checkers/main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include "cBoard.h"
 #include "Game.h"
-using namespace std;
 
 #define CUTOFF 10
+#define EASY 2
+#define MEDIUM 5
+#define HARD 10
 
 int maxLevel = 0;
 int totalNodes = 0;
 int maxPrune = 0;
 int minPrune = 0;
+int cutoff = CUTOFF;
+bool ord = true;
 
 void playCheckers();
 bool playerMove(bool player, cBoard *cgame, Game* game);
@@ -20,6 +24,11 @@ int minValue(cBoard* cgame, int alpha, int beta, int level);
 void AIStats();
 void test(cBoard* cgame, bool player);
 
+void playerTurn(cBoard* cgame, Game* game);
+void aiTurn(cBoard* &cgame, Game* game);
+
+void intro();
+
 int main() 
 {
 	playCheckers();
@@ -29,13 +38,17 @@ int main()
 
 void playCheckers()
 {
+	intro();
+
+	std::cout << std::endl << "Loading Game..." << std::endl;
+
+
 	cBoard* cgame = new cBoard();
 	Game *game = nullptr;
 	game = new Game();
 
 	game->init("GameEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 720, 720, false);
 
-	//Ask if player wants to move first or second
 
 	while (game->running())
 	{
@@ -43,45 +56,39 @@ void playCheckers()
 		game->update();
 		game->render(cgame);
 
-		if (cgame->availMoves(true))
+		std::cout << std::endl << std::endl;
+
+		if (ord)
 		{
-			game->renderJumps(cgame, true);
-			while (!playerMove(true, cgame, game))	//Human Player Move
-			{
-				game->renderJumps(cgame, true);
-				cout << "Invalid play" << endl;
-			}
-			game->render(cgame);
+			playerTurn(cgame, game);
+			aiTurn(cgame, game);
 		}
 		else
-			cout << "Player Move Forfeited, No Moves Available!" << endl;
-		
-		if (cgame->availMoves(false))
 		{
-			cout << "AI is thinking..." << endl;
-			ABSearch(cgame);
-			AIStats();
-			game->render(cgame);
+			aiTurn(cgame, game);
+			playerTurn(cgame, game);
 		}
-		else
-			cout << "AI Move Forfeited, No Moves Available!" << endl;
 
 		if (cgame->isEnd())
 		{
-			cout << endl << endl << "Game Finished" << endl;
+			std::cout << std::endl << std::endl << "Game Finished" << std::endl;
 			int result = cgame->utilEval();
 			if (result > 0)
-				cout << "AI Won!" << endl;
+				std::cout << "AI Won!" << std::endl;
 			else if (result < 0)
-				cout << "Player Won!" << endl;
+				std::cout << "Player Won!" << std::endl;
 			else
-				cout << "Draw!" << endl;
+				std::cout << "Draw!" << std::endl;
 			
 			break;
 		}
 	}
 
-	game->clean();
+	int v;
+	std::cout << "Enter 0 to continue" << std::endl;
+	std::cin >> v;
+	if (v == 0)
+		game->clean();
 }
 
 bool playerMove(bool player, cBoard *cgame, Game* game)
@@ -89,12 +96,12 @@ bool playerMove(bool player, cBoard *cgame, Game* game)
 	char piece, cmp;
 	if (player)
 	{
-		cout << "Player Move" << endl;
+		std::cout << "Player Move" << std::endl;
 		piece = 'B';
 	}
 	else
 	{
-		cout << "AI Move" << endl;
+		std::cout << "AI Move" << std::endl;
 		piece = 'W';
 	}
 	int x, y, newx, newy;
@@ -103,11 +110,7 @@ bool playerMove(bool player, cBoard *cgame, Game* game)
 	if ((cmp != '-') && (cmp != '_') && (cmp == piece))
 	{
 		game->renderMoves(cgame, player, x, y);
-
-		cout << "Old Coordinates:\t" << x << "\t" << y << endl;
-
 		game->mouseSelect(newx, newy);
-		cout << "New Coordinates:\t" << newx << "\t" << newy << endl;
 		if (!cgame->validMove(player, x, y, newx, newy))
 		{
 			game->render(cgame);
@@ -116,14 +119,16 @@ bool playerMove(bool player, cBoard *cgame, Game* game)
 		else
 		{
 			cgame->movePiece(player, x, y, newx, newy);
+			std::cout << "Old Coordinates:\t" << x << "\t" << y << std::endl;
+			std::cout << "New Coordinates:\t" << newx << "\t" << newy << std::endl;
 			cgame->printBoard();
-			cout << endl;
+			std::cout << std::endl;
 			return true;
 		}
 	}
 	else
 	{
-		cout << "Invalid Piece" << endl;
+		std::cout << "Invalid Piece" << std::endl;
 		return false;
 	}
 }
@@ -144,15 +149,15 @@ void ABSearch(cBoard* &cgame)
 
 int maxValue(cBoard* cgame, int alpha, int beta, int level)
 {
+	maxLevel = level;
 	totalNodes++;
-	if (cgame->isEnd() || (level == CUTOFF))
+	if (cgame->isEnd() || (level == cutoff))
 		return cgame->utilEval();
 
-	maxLevel = level;
-	cout << level << endl;
+	//std::cout << level << std::endl;
 	int v = -12;
 	int cmp;
-	queue<cBoard*> actions;
+	std::queue<cBoard*> actions;
 	cgame->genActions(false, actions);
 	cBoard* temp;
 	while (!actions.empty())
@@ -180,15 +185,15 @@ int maxValue(cBoard* cgame, int alpha, int beta, int level)
 
 int minValue(cBoard* cgame, int alpha, int beta, int level)
 {
+	maxLevel = level;
 	totalNodes++;
-	if (cgame->isEnd() || (level == CUTOFF))
+	if (cgame->isEnd() || (level == cutoff))
 		return cgame->utilEval();
 
-	maxLevel = level;
-	cout << level << endl;
+	//std::cout << level << std::endl;
 	int v = 12;
 	int cmp;
-	queue<cBoard*> actions;
+	std::queue<cBoard*> actions;
 	cgame->genActions(true, actions);
 	cBoard* temp;
 	while (!actions.empty())
@@ -215,7 +220,7 @@ int minValue(cBoard* cgame, int alpha, int beta, int level)
 }
 
 void test(cBoard* cgame, bool player) {
-	queue<cBoard*> actions;
+	std::queue<cBoard*> actions;
 
 	cgame->genActions(player, actions);
 
@@ -225,16 +230,97 @@ void test(cBoard* cgame, bool player) {
 	{
 		temp = actions.front();
 		temp->printBoard();
-		cout << endl;
+		std::cout << std::endl;
 		actions.pop();
 	}
 }
 
 void AIStats() 
 {
-	cout << endl << "AI Statistics:" << endl;
-	cout << "Max Depth:\t" << maxLevel << endl;
-	cout << "Total Nodes:\t" << totalNodes << endl;
-	cout << "Max Prune:\t" << maxPrune << endl;
-	cout << "Min Prune:\t" << minPrune << endl;
+	std::cout << std::endl << "AI Statistics:" << std::endl;
+	std::cout << "Max Depth:\t" << maxLevel << std::endl;
+	std::cout << "Total Nodes:\t" << totalNodes << std::endl;
+	std::cout << "Max Prune:\t" << maxPrune << std::endl;
+	std::cout << "Min Prune:\t" << minPrune << std::endl;
+}
+
+void playerTurn(cBoard* cgame, Game* game)
+{
+	if (cgame->availMoves(true))
+	{
+		game->renderJumps(cgame, true);
+		while (!playerMove(true, cgame, game))	//Human Player Move
+		{
+			game->renderJumps(cgame, true);
+			std::cout << "Invalid play" << std::endl;
+		}
+		game->render(cgame);
+	}
+	else
+		std::cout << "Player Move Forfeited, No Moves Available!" << std::endl;
+}
+
+void aiTurn(cBoard* &cgame, Game* game)
+{
+	if (cgame->availMoves(false))
+	{
+		std::cout << std::endl << "AI is thinking..." << std::endl;
+		ABSearch(cgame);
+		cgame->printBoard();
+		AIStats();
+		game->render(cgame);
+	}
+	else
+		std::cout << "AI Move Forfeited, No Moves Available!" << std::endl;
+}
+
+void intro()
+{
+	std::cout << "Welcome to AI Mini-Checkers!" << std::endl;
+	std::cout << "Please choose level of difficulty (1, 2, or 3):" << std::endl;
+
+	int ans = 0;
+	while ((ans != 1) && (ans != 2) && (ans != 3))
+	{
+		std::cin >> ans;
+		switch (ans)
+		{
+		case 1:
+			std::cout << "EASY Selected..." << std::endl;
+			cutoff = EASY;
+			break;
+		case 2:
+			std::cout << "MEDIUM Selected..." << std::endl;
+			cutoff = MEDIUM;
+			break;
+		case 3:
+			std::cout << "HARD Selected..." << std::endl;
+			cutoff = HARD;
+			break;
+		default:
+			std::cout << "Please choose a valid selection:" << std::endl;
+			break;
+		}
+	}
+
+	int ordans = 0;
+	std::cout << std::endl << "Are you going first or second? (1 or 2):" << std::endl;
+	while ((ordans != 1) && (ordans != 2))
+	{
+		std::cin >> ordans;
+		switch (ordans)
+		{
+		case 1:
+			std::cout << "Player Moves First!" << std::endl;
+			ord = true;
+			break;
+		case 2:
+			std::cout << "AI Moves First!" << std::endl;
+			ord = false;
+			break;
+		default:
+			std::cout << "Please enter a valid answer:" << std::endl;
+			break;
+		}
+	}
 }
